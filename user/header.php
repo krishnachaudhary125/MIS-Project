8bff51c69d152f006b8aef52bc5306274f00897c
krishnachaudhary125 <krishnachaudhary0125@gmail.com>
2025-04-30 20:02:28 +0545

    updated header

--- user/header.php
+++ user/header.php
@@ -6,11 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
 <body>
     <header>
-        <div class="navigation">
+        <nav class="navbar">
+            <div class="logo">
+                <img src="../photos/main-logo.png" alt="Logo">
+            </div>
             <ul>
                 <li><a href="index.php">Home</a></li>
                 <li><a href="#">Product</a></li>
@@ -18,5 +22,9 @@
                 <li><a href="register.php">Register</a></li>
                 <li><a href="login.php">Login</a></li>
             </ul>
-        </div>
+            <div class="search">
+                <input type="text" value="" placeholder="Search...">
+                <button type="submit"><i class="fa fa-search"></i></button>
+            </div>
+        </nav>
     </header>
--- user/register.php
+++ user/register.php
@@ -4,9 +4,21 @@ include "header.php";
 
 <div class="register-form">
     <form action="" method="post">
-        <h1>Register Form</h1>
         <div class="register-field">
-
+            <input type="text" name="fname" value="" placeholder="Full Name">
         </div>
+        <div class="register-field">
+            <input type="text" name="phone" value="" placeholder="Mobile Number">
+        </div>
+        <div class="register-field">
+            <input type="email" name="email" value="" placeholder="E-Mail">
+        </div>
+        <div class="register-field">
+            <input type="password" name="psw" value="" placeholder="Password">
+        </div>
+        <div class="register-field">
+            <input type="password" name="cpsw" value="" placeholder="Confirm Password">
+        </div>
+        <button type="submit">Register</button>
     </form>
 </div>
