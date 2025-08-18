d6124c0c1be89d5ae820da9fc68a260c14902ec0
krishnachaudhary125 <krishnachaudhary0125@gmail.com>
2025-08-18 22:44:58 +0545

    edited condition

--- admin/Action/admin_delete.php
+++ admin/Action/admin_delete.php
@@ -2,7 +2,7 @@
 session_start();
 include '../../Database/connection.php';
 
-if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'owner' || $_SESSION['role'] !== 'admin') {
+if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'owner' && $_SESSION['role'] !== 'admin') {
     echo "<script>alert('Unauthorized page!');</script>";
     echo "<script>window.location.href = '../../user/index.php';</script>";
     exit();
--- admin/header.php
+++ admin/header.php
@@ -1,7 +1,7 @@
 <?php 
 session_start();
 
-if (!isset($_SESSION['role']) ||$_SESSION['role'] !== 'owner' || $_SESSION['role'] !== 'admin') {
+if (!isset($_SESSION['role']) ||$_SESSION['role'] !== 'owner' && $_SESSION['role'] !== 'admin') {
     echo "<script>alert('Unauthorized page!');</script>";
     echo "<script>window.location.href = '../user/index.php';</script>";
     exit();
